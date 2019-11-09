e1c4f2fa96bb0ea7c4964632234e14c716588c06
Riccardo <riccardo.fuda@gmx.de>
2019-11-09 16:48:33 +0100

    GuardBee Manager

---
+++ BeeeHave/Assets/Scripts/Hive/DefenseBeeManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenseBeeManager : MonoBehaviour
+{
+    [SerializeField] GameObject GuardbeePrefab;
+    [SerializeField] Transform[] SpawnPoints;
+
+    [HideInInspector]
+    public int amountOfCurrentBees = 0;
+    private int SpCounter = 0, children;
+
+    void Start()
+    {
+        
+           
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            SpawnGuardBee();
+        }
+    }
+
+    public void SpawnGuardBee()
+    {
+        GameObject Bee = Instantiate(GuardbeePrefab, SpawnPoints[SpCounter].position, Quaternion.identity);
+        SpCounter++;
+    }
+}
