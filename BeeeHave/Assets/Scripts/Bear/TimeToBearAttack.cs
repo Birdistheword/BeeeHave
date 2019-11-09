407b40663d369cf2f4571a020d69b9b3e6e74e2a
Riccardo <riccardo.fuda@gmx.de>
2019-11-09 16:15:57 +0100

    Bear ROutine

--- BeeeHave/Assets/Scripts/Bear/TimeToBearAttack.cs
+++ BeeeHave/Assets/Scripts/Bear/TimeToBearAttack.cs
@@ -6,25 +6,47 @@ using UnityEngine.UI;
 
 public class TimeToBearAttack : MonoBehaviour
 {
+    [SerializeField] GameObject bearPrefab;
     [SerializeField] Image img;
-    [SerializeField] float timeTillAttack = 0f;
+
+
+    float[] attackIntervals =  { 3f, 60f, 60f, 50f, 50f, 40f, 40f, 35f };
+
+    private float counter = 0f, timeTillAttack;
+    private bool countingTime = true;
+    public bool  sentAttack = false;
 
 
     void Start()
     {
-        
+        timeTillAttack = attackIntervals[0];
     }
 
 
     void Update()
     {
-        timeTillAttack -= Time.deltaTime;
 
-        if(timeTillAttack <= 0f)
+        if(countingTime)
         {
-            // Bear Attack!!
+            counter += Time.deltaTime;
+            Debug.Log(counter);
+            img.fillAmount = (timeTillAttack - counter) / timeTillAttack;
         }
 
-        img.fillAmount += timeTillAttack / 100f;
+        if ( counter >= timeTillAttack && !sentAttack )
+        {
+            bearPrefab.GetComponent<BearController>().startAttacking = true;
+            countingTime = false;
+            ChooseNewAttackInterval();
+            sentAttack = true;
+        }
+
+
+    }
+
+    private void ChooseNewAttackInterval ()
+    {
+        timeTillAttack = attackIntervals[Random.Range(0, attackIntervals.Length)];
+        Debug.Log("Time till next attack:" + timeTillAttack);
     }
 }
