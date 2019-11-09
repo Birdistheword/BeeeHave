0d50d00354f6e7969bdfa0cd822c8726be2cdaf1
Riccardo <riccardo.fuda@gmx.de>
2019-11-09 18:02:42 +0100

    BearController Fix

--- BeeeHave/Assets/Scripts/Bear/BearController.cs
+++ BeeeHave/Assets/Scripts/Bear/BearController.cs
@@ -7,7 +7,6 @@ public class BearController : MonoBehaviour
     [SerializeField]
     Transform BearTarget;
     [SerializeField]float movSpeed = 3f;
-<<<<<<< HEAD
     [SerializeField] GameObject Hive;
 
 
@@ -18,30 +17,34 @@ public class BearController : MonoBehaviour
     private int[] HealthPool = { 1, 1, 2, 2, 2, 3, 3, 3, 4 };
     private int currentHealth;
     private GameStates GS;
-=======
-    [HideInInspector]
-    public bool startAttacking = false, isAttacking = false;
->>>>>>> b5d1eb37b54ca3fa7783d607ccb0b935549698ae
 
     void Start()
     {
-        
+        GS = GameObject.FindObjectOfType<GameStates>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // At Start, Check for Start Attack
         if (GS.STATE == GameStates.GameState.BearStartAttack) StartAttack();
 
         if(isAttacking)
         {
             transform.position = Vector3.MoveTowards(transform.position, BearTarget.position, movSpeed * Time.deltaTime);
-<<<<<<< HEAD
 
             if(Vector3.Distance(BearTarget.position, transform.position) <= 1f)
             {
                 GS.STATE = GameStates.GameState.BearAtHive;
             }
+        }
+
+
+        // When Bear is at Hive, Check if it has 0 HP and if it does, RETREAT
+        if(GS.STATE == GameStates.GameState.BearAtHive && currentHealth <= 0)
+        {
+            GS.STATE = GameStates.GameState.BearRetreat;
         }
 
         if (GS.STATE == GameStates.GameState.DamagePhase && didDamage == false)
@@ -54,22 +57,35 @@ public class BearController : MonoBehaviour
 
 
 
-=======
-            Debug.Log("IsAttacking");
-        }
->>>>>>> b5d1eb37b54ca3fa7783d607ccb0b935549698ae
     }
 
+    //Set attacking to true and let bear approach Hive
     private void StartAttack()
     {
-<<<<<<< HEAD
         GS.STATE = GameStates.GameState.BearMovingToHive;
         SetHealth();
-=======
-        Debug.Log("StartAttacking");
-        startAttacking = false;
->>>>>>> b5d1eb37b54ca3fa7783d607ccb0b935549698ae
         isAttacking = true;
 
     }
+
+    //Set Health, 
+    // First time -> 1
+    // Next Times Random from Array
+    private void SetHealth ()
+    {
+        if (firstAttack)
+        {
+            currentHealth = 1;
+            firstAttack = false;
+        }
+           
+        else
+            currentHealth = HealthPool[Random.Range(0, HealthPool.Length)];
+    }
+
+    public void TakeDamage(int _damage)
+    {
+        currentHealth -= _damage;
+    }
+
 }
