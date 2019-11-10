b64015711cdb5a3b6abdd2e66dd84611e7825e1a
Tadeáš Anděl <tadeas.andel@gmail.com>
2019-11-10 11:47:44 +0100

    Stat refactoring

--- BeeeHave/Assets/Scripts/Bee/Stats/StatManager.cs
+++ BeeeHave/Assets/Scripts/Bee/Stats/StatManager.cs
@@ -8,19 +8,35 @@ public class StatManager : MonoBehaviour
   [SerializeField] int efficiencyStatLevel = 1;
   [SerializeField] int PollenCarryStatLevel = 1;
 
+  [SerializeField] int pollenEfficiencyStatValue = 2;
+
+  [SerializeField] int pollenCarryStatValue = 2;
+
+  PlayerMovementSimple playerMovementSimple;
+  PollenManager pollenManager;
+
+  private void Start()
+  {
+    playerMovementSimple.GetComponent<PlayerMovementSimple>();
+    pollenManager.GetComponent<PollenManager>();
+  }
+
   public void AddSpeedStat()
   {
     speedStatLevel++;
+    //playerMovementSimple.
   }
 
   public void AddEfficiencyStat()
   {
     efficiencyStatLevel++;
+    pollenManager.IncreasePollenEfficiency(pollenEfficiencyStatValue);
   }
 
   public void AddCarryStat()
   {
     PollenCarryStatLevel++;
+    pollenManager.IncreaseMaxAmountOfPollen(pollenCarryStatValue);
   }
 
   public int GetSpeedStatLevel()
